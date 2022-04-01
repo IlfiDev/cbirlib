c9e7f7c8c7b25991d038612cac59d99cae177ccc
ilfiDev <ilyafillipov2@gmail.com>
2022-04-01 22:08:16 +0300

    Added layer, Activation and loss

--- cbirlib/CBIR.py
+++ cbirlib/CBIR.py
@@ -1,1 +1,71 @@
-#
+import numpy as np
+
+
+class DenseLayer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+
+class ActivationSoftmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+
+        for index, (single_output, single_dvalues) in \
+            enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(single_output) - \
+                np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        self.dinputs = -y_true / dvalues
+        self.dinputs = self.dinputs / samples
--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+from cbirlib.CBIR import DenseLayer, ActivationReLU, ActivationSoftmax, Loss_CategoricalCrossentropy
+import nnfs
+import matplotlib.pyplot as plt
+import numpy as np
+from nnfs.datasets import spiral_data
+from nnfs.datasets import vertical_data
 
+nnfs.init()
+
+X, y = vertical_data(samples=100, classes=3)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
+plt.show()
 if __name__ == '__main__':
 
-#
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dense1 = DenseLayer(2, 3)
+    dense2 = DenseLayer(3, 3)
+
+    activation1 = ActivationReLU()
+    activation2 = ActivationSoftmax()
+    loss_function = Loss_CategoricalCrossentropy()
+    dense1.forward(X)
+
+    lowest_loss = 9999999
+    best_dense1_weights = dense1.weights.copy()
+    nest_dense1_biases = dense1.biases.copy()
+    best_dense2_weights = dense2.weights.copy()
+    nest_dense2_biases = dense2.biases.copy()
+
+    for iteration in range(10000):
+        dense1.weights += 0.05 * np.random.randn(2, 3)
+        dense1.biases += 0.05 * np.random.randn(1, 3)
+        dense2.weights += 0.05 * np.random.randn(3, 3)
+        dense2.biases += 0.05 * np.random.randn(1, 3)
+
+        dense1.forward(X)
+        activation1.forward(dense1.output)
+        dense2.forward(activation1.output)
+        activation2.forward(dense2.output)
+
+        loss = loss_function.calculate(activation2.output, y)
+        predictions = np.argmax(activation2.output, axis=1)
+        accuracy = np.mean(predictions == y)
+
+        if loss < lowest_loss:
+            print("New set of weights found, iteration:", iteration, "loss:", loss, "acc:", accuracy)
+            best_dense1_weights = dense1.weights.copy()
+            best_dense1_biases = dense1.biases.copy()
+            best_dense2_weights = dense2.weights.copy()
+            best_dense2_biases = dense2.biases.copy()
+            lowest_loss = loss
