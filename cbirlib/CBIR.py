0f7b53c1d174a246df417fc015b17cff073fc5f9
ilfiDev <ilyafillipov2@gmail.com>
2022-04-29 21:31:57 +0300

    Added Convolution

--- cbirlib/CBIR.py
+++ cbirlib/CBIR.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-
+from PIL import Image
+from scipy import signal
+import matplotlib.pyplot as plt
 class DenseLayer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -9,21 +10,31 @@ class DenseLayer:
     def forward(self, inputs):
         self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.output = self.output.clip(-1000, 1000)
+        #print(np.mean(self.output), "f", "dense")
 
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
         self.dinputs = np.dot(dvalues, self.weights.T)
+        self.dinputs = self.dinputs.clip(-1000, 1000)
+        #print(np.mean(self.dweights), "b", "dense")
 
 
 class ActivationReLU:
     def forward(self, inputs):
         self.inputs = inputs
+        self.output = np.empty_like(inputs)
         self.output = np.maximum(0, inputs)
+        #self.output.clip(-100, 100)
+        #print(np.mean(self.output), "f", "relu")
 
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
-        self.dinputs[self.inputs <= 0] = 0
+        #self.dinputs[self.inputs <= 0] = 0
+        self.dinputs.clip(min=0)
+        #self.dinputs.clip(-100, 100)
+        #print(np.mean(self.inputs), "b", "relu")
 
 
 class ActivationSoftmax:
@@ -31,7 +42,10 @@ class ActivationSoftmax:
         self.inputs = inputs
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
         self.output = probabilities
+        self.output = self.output.clip(-100, 100)
+        #print(np.mean(self.output), "f", "actSoftmax")
 
     def backward(self, dvalues):
         self.dinputs = np.empty_like(dvalues)
@@ -40,6 +54,8 @@ class ActivationSoftmax:
             single_output = single_output.reshape(-1, 1)
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
             self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+        self.dinputs = self.dinputs.clip(-100, 100)
+            #print(np.mean(self.dinputs), "b", "actSoftmax")
 
 
 class Loss:
@@ -70,6 +86,7 @@ class Loss_CategoricalCrossentropy(Loss):
 
         self.dinputs = -y_true / dvalues
         self.dinputs = self.dinputs / samples
+        #self.dinputs = self.dinputs.clip(-10, 10)
 
 
 class Activation_softmax_Loss_CategoricalCrossentropy():
@@ -80,6 +97,7 @@ class Activation_softmax_Loss_CategoricalCrossentropy():
     def forward(self, inputs, y_true):
         self.activation.forward(inputs)
         self.output = self.activation.output
+        #print(np.mean(self.output), "f", "loss_cacteg")
         return self.loss.calculate(self.output, y_true)
 
     def backward(self, dvalues, y_true):
@@ -89,6 +107,8 @@ class Activation_softmax_Loss_CategoricalCrossentropy():
         self.dinputs = dvalues.copy()
         self.dinputs[range(samples), y_true] -= 1
         self.dinputs = self.dinputs / samples
+        #self.dinputs = self.dinputs.clip(-10, 10)
+        #print(np.mean(self.dinputs), "b", "loss_categ")
 
 class SGD_Optimizer:
     def __init__(self, learning_rate=1., decay=0., momentum=0.):
@@ -109,8 +129,8 @@ class SGD_Optimizer:
                 layer.weight_momentums = np.zeros_like(layer.weights)
                 layer.bias_momentums = np.zeros_like(layer.biases)
 
-            weight_updates = self.momentum * layer.weight_momentums - \
-                self.current_learning_rate * layer.dweights
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+
             layer.weight_momentums = weight_updates
 
             bias_updates = self.momentum * layer.bias_momentums - \
@@ -119,8 +139,74 @@ class SGD_Optimizer:
         else:
             weight_updates = -self.current_learning_rate * layer.dweights
             bias_updates = -self.current_learning_rate * layer.dbiases
+
         layer.weights += weight_updates
         layer.biases += bias_updates
 
     def post_update_params(self):
         self.iterations += 1
+
+class Convolution:
+    def __init__(self, input_shape, kernel_size, depth):
+        input_depth, input_height, input_width = input_shape
+        self.depth = depth
+        self.input_depth = input_depth
+        self.input_shape = input_shape
+        self.output_shape = (depth, input_height - kernel_size + 1, input_width - kernel_size + 1)
+        self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
+        self.weights = np.random.randn(*self.kernels_shape)
+        self.biases = np.random.randn(*self.output_shape)
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.copy(self.biases)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.output[i] += signal.correlate2d(self.inputs[j], self.weights[i, j], "valid")
+        self.output.clip(-100, 100)
+        return self.output
+
+    def backward(self, output_gradient):
+        self.dweights = np.zeros(self.kernels_shape)
+        self.dinputs = np.zeros(self.input_shape)
+        self.dbiases = output_gradient
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.dweights[i, j] = signal.correlate2d(self.inputs[j], output_gradient[i], "valid")
+                self.dinputs[j] += signal.convolve2d(output_gradient[i], self.weights[i, j], "full")
+        self.dinputs = self.dinputs.clip(-100, 100)
+        #self.weights = self.weights.clip(-10, 10)
+        return self.dweights
+
+
+class Max_pool:
+    def forward(self, matrix):
+        self.matrix = matrix
+        self.output = np.zeros((matrix.shape[0], int(matrix.shape[1] / 2), int(matrix.shape[2] / 2)))
+        self.mask = np.zeros_like(self.matrix)
+        for dim in range(matrix.shape[0]):
+            for i in range(0, matrix.shape[1], 2):
+                for j in range(0, matrix.shape[2], 2):
+
+                    if j >= matrix.shape[2] - 2 or i >= matrix.shape[1] - 2:
+                        break
+                    self.output[dim, i//2, j//2] = np.max(matrix[dim, i:i + 1, j:j + 1])
+                    self.mask[dim, i:i + 1, j:j + 1] = np.max(matrix[dim, i:i + 1, j:j + 1])
+
+
+            # plt.imshow(self.output)
+            # plt.show()
+        #print(np.mean(self.output), "f", "maxpool")
+        self.output = self.output.clip(-10, 10)
+        return self.output
+
+    def backward(self, inputs):
+        self.dinputs = np.copy(self.mask)
+        for dim in range(inputs.shape[0]):
+            for i in range(inputs.shape[1]):
+                for j in range(inputs.shape[2]):
+                    if self.dinputs[dim, i * 2:i* 2 + 1, j*2:j*2 + 1] > 0:
+                        self.dinputs[dim, i * 2:i * 2 + 1, j * 2:j * 2 + 1] = inputs[dim, i, j]
+        #print(np.mean(self.dinputs), "b", "max_pool")
+        self.dinputs = self.dinputs.clip(-10, 10)
+        return self.dinputs
--- main.py
+++ main.py
@@ -1,45 +1,82 @@
+
+
 from cbirlib.CBIR import *
 import nnfs
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import scipy
 from nnfs.datasets import spiral_data
+from PIL import Image
+from keras.datasets import mnist
 from nnfs.datasets import vertical_data
 
 nnfs.init()
 
-X, y = spiral_data(samples=100, classes=3)
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
-plt.show()
+#X, y = spiral_data(samples=100, classes=3)
+#plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')'
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#print(np.shape(x_train))
 if __name__ == '__main__':
-    optimizer = SGD_Optimizer(decay=1e-3, momentum=0.9)
-    dense1 = DenseLayer(2, 64)
+    X, y = spiral_data(100, 3)
+    print(X.shape)
+    #print(np.mean(self.dweights), "b", "actSoftmax")
+    optimizer = SGD_Optimizer(decay=1e-3, momentum=0.9, learning_rate=1)
+    convolution1 = Convolution((1, 28, 28), 3, 3)
     activation1 = ActivationReLU()
-    dense2 = DenseLayer(64, 3)
+    max_pool1 = Max_pool()
+    convolution2 = Convolution((1, 13, 13), 3, 3)
+    activation2 = ActivationReLU()
+    max_pool2 = Max_pool()
+    dense1 = DenseLayer(75, 75)
+    activation3 = ActivationReLU()
+    dense2 = DenseLayer(75, 10)
     loss_activation = Activation_softmax_Loss_CategoricalCrossentropy()
 
     for epoch in range(10001):
-        dense1.forward(X)
-        activation1.forward(dense1.output)
-        dense2.forward(activation1.output)
-        loss = loss_activation.forward(dense2.output, y)
+        convolution1.forward(x_train)
+        activation1.forward(convolution1.output)
+        max_pool1.forward(activation1.output)
+        convolution2.forward(max_pool1.output)
+        activation2.forward(convolution2.output)
+        max_pool2.forward(activation2.output)
+        dense1.forward(max_pool2.output.reshape(1, 75))
+        activation3.forward(dense1.output)
+        dense2.forward(activation3.output)
+        lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        loss = loss_activation.forward(dense2.output, y_train)
+        #print(loss_activation.output)
         predictions = np.argmax(loss_activation.output, axis=1)
-        if len(y.shape) == 2:
-            y = np.argmax(y, axis=1)
-        #np.mean = среднее арифметическое
-        accuracy = np.mean(predictions == y)
-
+        if len(y_train.shape) == 2:
+            y_train = np.argmax(y_train, axis=1)
+        accuracy = np.mean(predictions == y_train)
         if not epoch % 100:
+            plt.imshow(convolution2.output[0, :, :])
+            plt.show()
+            plt.imshow(convolution2.output[1, :, :])
+            plt.show()
+            plt.imshow(convolution2.output[2, :, :])
+            plt.show()
             print(f'{epoch}, ' +
                   f'acc: {accuracy:.3f}, ' +
                   f'loss: {loss:.3f}, ' +
                   f'lr: {optimizer.current_learning_rate}')
 
-        loss_activation.backward(loss_activation.output, y)
+        loss_activation.backward(loss_activation.output, y_train)
         dense2.backward(loss_activation.dinputs)
-        activation1.backward(dense2.dinputs)
-        dense1.backward(activation1.dinputs)
+        activation3.backward(dense2.dinputs)
+        dense1.backward(activation3.dinputs)
+        max_pool2.backward(dense1.dinputs.reshape((3, 5, 5)))
+        activation2.backward(max_pool2.dinputs)
+        convolution2.backward(activation2.dinputs)
+        max_pool1.backward(convolution2.dinputs)
+        activation1.backward(max_pool1.dinputs)
+        convolution1.backward(activation1.dinputs)
 
         optimizer.pre_update_params()
+        optimizer.update_params(convolution1)
+        optimizer.update_params(convolution2)
         optimizer.update_params(dense1)
         optimizer.update_params(dense2)
         optimizer.post_update_params()
+
